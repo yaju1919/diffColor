b6fc59ef350e1b6a61b1b08cfdd4b2bb0ebe8274
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-05-05 03:12:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-diffColor
 (function() {
     'use strict';
     var h = $("<div>").appendTo($("body")).css({
@@ -7,4 +6,36 @@ diffColor
     });
     $("<h1>",{text:"色の距離の計算"}).appendTo(h);
     $("<div>",{text:"aaaa"}).appendTo(h);
+    function addInputColor(title){
+        var hh = $("<div>",{text:title}).appendTo(h);
+        var show = $("<div>").appendTo(h);
+        var i = $("<input>",{type:"color"}).change(function(){
+            show.text($(this).val());
+            main();
+        });
+        return function(){ return i.val() };
+    }
+    h.append("<br>");
+    var a = addInputColor("色A"),
+        b = addInputColor("色B");
+    h.append("<br>");
+    h.append("<br>");
+    var result = $("<div>");
+    function orgRound(value, base) { // 四捨五入
+        return Math.round(value * base) / base;
+    }
+    function main(){
+        result.empty();
+        var aa = a(),
+            bb = b(),
+            str = "";
+        [
+            "RGB表色系でのユークリッド距離による色差の計算",
+            "XYZ表色系でのユークリッド距離による色差の計算",
+            "L*a*b*表色系でのユークリッド距離による色差の計算",
+            "CIEDE2000による色差の計算"
+        ].map(function(v,i){
+            $("<div>",{text:h}).append("<br>").append(orgRound(diffColor(a,b,3-i),0.001)).appendTo(result);
+        })
+    }
 })();
