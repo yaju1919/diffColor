39c8b2153ff930e4efde3ca5ba5c091cd7759c92
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-05-05 02:34:01 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+diffColor
+(function() {
+    'use strict';
+    var h = $("<div>").appendTo($("body")).css({
+        "text-align": "center",
+        padding: "1em"
+    });
+    $("<h1>",{text:"色の距離の計算"}).appendTo(h);
+    $("<div>",{text:"aaaa"}).appendTo(h);
+})();
